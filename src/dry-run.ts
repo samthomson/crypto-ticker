ab3178689cea5802cb28e0ab96fe245b0d353dcc
Sam Thomson <samt@samt.st>
2018-04-16 20:38:48 +0100

    rough mvp

--- src/dry-run.ts
+++ src/dry-run.ts
@@ -1,1 +1,41 @@
-console.log('dry-run')
+import { getCryptoUSDValue } from 'get-crypto-fiat-values'
+import * as mailgun from 'mailgun-js'
+
+const {
+    CURRENCIES,
+    EMAIL_TARGET,
+    MAILGUN_DOMAIN,
+    MAILGUN_KEY,
+} = process.env
+
+console.log('EMAIL_TARGET', EMAIL_TARGET)
+console.log('CURRENCIES', CURRENCIES)
+
+const dryrun = async () => {
+    const resultsCMCCurrencySnapshot:number = await getCryptoUSDValue(CURRENCIES || '') || -1
+
+    const literalPrice: string = `${CURRENCIES} value is $${resultsCMCCurrencySnapshot}`
+
+    const mailgunInstance = mailgun({
+        apiKey: MAILGUN_KEY || '',
+        domain: MAILGUN_DOMAIN || ''
+    })
+    const oMessageData: any = {
+        from: `${EMAIL_TARGET} <${EMAIL_TARGET}>`,
+        to: `${EMAIL_TARGET} <${EMAIL_TARGET}>`,
+        subject: literalPrice,
+        text: literalPrice,
+        html: literalPrice
+    }
+
+    mailgunInstance.messages().send(oMessageData, (err, body) => {
+        if (body) {
+            console.log('mail sent Ok ', body)
+        }
+        if (err) {
+            console.log('err sending mail, ', err)
+        }
+    })
+}
+
+dryrun()
