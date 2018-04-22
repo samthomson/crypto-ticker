2bd323400f94c973cc7110d980c9a752f82a3ee1
Sam Thomson <samt@samt.st>
2018-04-22 15:01:29 +0100

    convert and format as dkk

--- src/lib.ts
+++ src/lib.ts
@@ -1,5 +1,6 @@
 import { getCryptoUSDValue } from 'get-crypto-fiat-values'
 import * as mailgun from 'mailgun-js'
+import * as currencyConvert from 'currency-convert'
 
 const {
     CURRENCIES,
@@ -12,33 +13,36 @@ export const main = async () => {
     const result:number = await getCryptoUSDValue(CURRENCIES || '') || -1
 
     var formatter = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
         minimumFractionDigits: 2,
-    });
+    })
       
-    const formattedDollarAmmount: string = formatter.format(result); /* $2,500.00 */
 
-    const literalPrice: string = `${CURRENCIES} is at ${formattedDollarAmmount}`
+    const dkkAmount = await currencyConvert(result, 'USD', 'DKK')
+    const formattedAmmount: string = formatter.format(dkkAmount)
+    
+    const literalPrice: string = `${CURRENCIES} is at ${formattedAmmount} DKK`
 
     const mailgunInstance = mailgun({
         apiKey: MAILGUN_KEY || '',
         domain: MAILGUN_DOMAIN || ''
     })
-    const oMessageData: any = {
-        from: `crypto-ticker <noreply@fake.email>`,
-        to: `${EMAIL_TARGET} <${EMAIL_TARGET}>`,
-        subject: literalPrice,
-        text: literalPrice,
-        html: literalPrice
-    }
 
-    mailgunInstance.messages().send(oMessageData, (err, body) => {
-        if (body) {
-            console.log('mail sent Ok ')
-        }
-        if (err) {
-            console.log('err sending mail, ', err)
-        }
-    })
+    console.log(literalPrice)
+
+    // const oMessageData: any = {
+    //     from: `crypto-ticker <noreply@fake.email>`,
+    //     to: `${EMAIL_TARGET} <${EMAIL_TARGET}>`,
+    //     subject: literalPrice,
+    //     text: literalPrice,
+    //     html: literalPrice
+    // }
+
+    // mailgunInstance.messages().send(oMessageData, (err, body) => {
+    //     if (body) {
+    //         console.log('mail sent Ok ')
+    //     }
+    //     if (err) {
+    //         console.log('err sending mail, ', err)
+    //     }
+    // })
 }
